d906c34e2e5d75e11937868667e09ebe1d2e6230
Spkce <m15188159686@163.com>
2022-02-19 15:27:57 +0800

    update

--- link/linkReverse.cpp
+++ link/linkReverse.cpp
@@ -1,5 +1,5 @@
 #include "stdio.h"
-
+#include <iostream>
 struct singleNode_t
 {
 	singleNode_t* next;
@@ -27,39 +27,157 @@ public:
 		}
 		return newHead;
 	}
+
+	int linkSize(singleNode_t* head)
+	{
+		int i = 0;
+		while(head != NULL)
+		{
+			head = head->next;
+			i++;
+
+		}
+		return i;
+	}
+
+	singleNode_t* sort(singleNode_t* head)
+	{
+		if (head == NULL || head->next == NULL)
+		{
+			return head;
+		}
+
+		singleNode_t* pMin = head;
+		singleNode_t* pMinLast = NULL;
+
+		singleNode_t* pi = head;
+		singleNode_t* piLast = NULL;
+
+		while (pi->next != NULL)
+		{
+			printf("pi = %p pi->next = %p, pi->data = %d \n", pi, pi->next, pi->data);
+			singleNode_t* pj = pi->next;
+			singleNode_t* pjLast = pi;
+			pMin = pi->next;
+			pMinLast = pi;
+			while(pj != NULL)
+			{
+				if (pj->data < pMin->data)
+				{
+					pMinLast = pjLast;
+					pMin = pj;
+				}
+
+				pjLast = pj;
+				pj = pj->next;
+			}
+
+			if (pMin != pi)
+			{
+				printf("pMin = %p pMin->next = %p, pMin->data = %d \n", pMin, pMin->next, pMin->data);
+				printf("pMinLast = %p \n", pMinLast);
+				if (piLast != NULL)
+				{
+					piLast->next = pMin;
+				}
+				if (pMinLast != NULL)
+				{
+					pMinLast->next = pi;
+				}
+				
+				singleNode_t* temp = pi->next;
+				pi->next = pMin->next;
+				pMin->next = temp;
+
+				if (pi == head)
+				{
+					head = pMin;
+				}
+
+				pi = pMin->next;
+				piLast = pMin;
+			}
+			else
+			{
+				pi = pi->next;
+				piLast = pi;
+			}
+		}
+
+		return head;
+	}
+
+	singleNode_t* merge(singleNode_t* a, singleNode_t* b)
+	{
+		
+	}
 };
 
 
 int main(int argc, char const *argv[])
 {
-	singleNode_t* pHead = new singleNode_t;
-	pHead->data = 0;
-	pHead->next = NULL;
-
-	singleNode_t* p = pHead;
-	for(int i = 1; i < 10; i++)
+	if (0)
 	{
-		p->next = new singleNode_t;
-		p = p->next;
-		p->data = i;
-		p->next = NULL;
+		singleNode_t* pHead = new singleNode_t;
+		pHead->data = 0;
+		pHead->next = NULL;
+		singleNode_t *p = pHead;
+		for (int i = 1; i < 10; i++)
+		{
+			p->next = new singleNode_t;
+			p = p->next;
+			p->data = i;
+			p->next = NULL;
+		}
+
+		p = pHead;
+		while (p != NULL)
+		{
+			printf("p = %p p->next = %p, p->data = %d \n", p, p->next, p->data);
+			p = p->next;
+		}
+		printf("\n");
+		printf("reverse:\n");
+		singlelist a;
+		p = a.reverse(pHead);
+		while (p != NULL)
+		{
+			printf("p = %p p->next = %p, p->data = %d \n", p, p->next, p->data);
+			p = p->next;
+		}
 	}
 
-	p = pHead;
-	while (p != NULL)
 	{
-		printf ("p = %p p->next = %p, p->data = %d \n", p,  p->next, p->data);
-		p = p->next;
-	}
-	printf("\n");
-	singlelist a;
-	p = a.reverse(pHead);
-	while (p != NULL)
-	{
-		printf ("p = %p p->next = %p, p->data = %d \n", p,  p->next, p->data);
-		p = p->next;
-	}
+		singleNode_t *pHead = new singleNode_t;
+		pHead->data = 0;
+		pHead->next = NULL;
+		singleNode_t *p = pHead;
+		for (int i = 1; i < 10; i++)
+		{
+			
+			p->next = new singleNode_t;
+			p = p->next;
+			p->data = 10-i;
+			p->next = NULL;
+		}
 
-	printf("\n");
+		p = pHead;
+		while (p != NULL)
+		{
+			printf("p = %p p->next = %p, p->data = %d \n", p, p->next, p->data);
+			p = p->next;
+		}
+
+		printf("sort:\n");
+		singlelist a;
+		p = a.sort(pHead);
+		while (p != NULL)
+		{
+			printf("p = %p p->next = %p, p->data = %d \n", p, p->next, p->data);
+			p = p->next;
+		}
+		printf("\n");
+	}
 	return 0;
 }
+
